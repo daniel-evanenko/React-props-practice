274a7ee4d69960ccdd9b49aa346cd234d8506e3d
daniel <daniel.evanenko@gmail.com>
2022-06-22 12:21:10 +0300

    Added card, avatar, detail components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Card from "./Card";
 import Heading from "./Heading";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 function App() {
   return (
     <div>
       <Heading />
+      <Avatar imgURL="https://media-exp1.licdn.com/dms/image/C4E03AQEHf6RY9bxgdw/profile-displayphoto-shrink_800_800/0/1649763052505?e=1661385600&v=beta&t=l3QPlN6hYIVX9qRG7LarD5ExcTEinMxuC1tDUkcNvWU" />
       <Card 
       name={contacts[0].name}
       imgURL={contacts[0].imgURL}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
 import React from "react"
+import Avatar from "./Avatar";
+import Details from "./Detail";
 function Card(props){
 
     return <div className="card">
             <div className="top">
             <h2 className="name">{props.name}</h2>
-            <img className="circle-img"
-                src={props.imgURL}
-                alt="avatar_img"
-            />
+            <Avatar imgURL = {props.imgURL}/>
             </div>
             <div className="bottom">
-            <p className="info">{props.phone}</p>
-            <p className="info">{props.email}</p>
+            <Details detailinfo={props.phone} />
+            <Details detailinfo={props.email} />
             </div>
             </div>
 }
